0f325d5187b80181c19e7a16d49bfe7d264a58a9
Aline Rosa <78911442+Aline-rs@users.noreply.github.com>
2022-10-20 00:52:53 +0000

    Novas informações

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ Comentário em bloco
 #
 #print() = comando de saída
 print("Olá mundo!")
+
+#Quando quiser guardar uma string usamos variaveis.
+
+nome = "Aline Rosa"
+idade = 23
+print(nome)
+print(idade)
