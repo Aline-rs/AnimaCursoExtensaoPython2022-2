7115742514730746339dbdaa64b1f08e6d918e45
Aline Rosa <78911442+Aline-rs@users.noreply.github.com>
2022-10-20 01:19:33 +0000

    Mais anotações

--- main.py
+++ main.py
@@ -13,3 +13,10 @@ nome = "Aline Rosa"
 idade = 23
 print(nome)
 print(idade)
+
+#str -> FUNÇÃO DE CONVERSÃO DE INTEIRO PARA STRING
+
+print("Minha idade é: "+str(idade))
+print(F"Minha idade é: {idade}")
+print("Minha idade é: {}".format(idade))
+print("Minha idade é: {} e tenho {}".format(nome, idade))
